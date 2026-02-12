5bb863df2fd4fa5018cb9a0821796390a6a15bc7
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-02-12 17:17:32 +0700

    complete decision tree code

--- assignment/assignment_1/decision_tree.py
+++ assignment/assignment_1/decision_tree.py
@@ -52,56 +52,6 @@ def read_data(train_path, test_path):
     
     return x_train, y_train, x_test, passenger_id
         
-def compute_entropy(p):
-    if p == 0 or p == 1:
-        return 0
-    return -(p * np.log2(p) + (1 - p) * np.log2(1 - p))
-
-def compute_score(x, y):
-    thresholds = np.unique(x)
-    n_thresholds = len(thresholds)
-    n_samples = len(y)
-    best_compute_score, best_compute_threshold = float("inf"), None
-        
-    for i in range(n_thresholds - 1):
-        threshold = (thresholds[i] + thresholds[i + 1]) / 2
-        left = y[x <= threshold]
-        right = y[x > threshold]
-        n_left = len(left)
-        n_right = len(right)
-        
-        if n_left == 0 or n_right == 0:
-            continue
-        
-        w_left = n_left / n_samples
-        w_right = n_right / n_samples
-        
-        # y values are 0 and 1
-        p_left = left.mean()
-        p_right = right.mean()
-        
-        score = w_left * compute_entropy(p_left) + w_right * compute_entropy(p_right)
-        if score < best_compute_score:
-            best_compute_score = score
-            best_compute_threshold = threshold
-            
-    return best_compute_score, best_compute_threshold
-    
-def best_split(X, y):
-    best_feature, best_threshold = None, None
-    best_score = float("inf")
-    
-    n_features = X.shape[1]
-    
-    for feature in range(n_features):
-        score, threshold = compute_score(X[:, feature], y)
-        if score < best_score:
-            best_score = score
-            best_feature = feature
-            best_threshold = threshold
-    
-    return best_feature, best_threshold
-
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
         self.feature = feature
@@ -109,55 +59,122 @@ class Node:
         self.left = left
         self.right = right
         self.value = value
+
+class DecisionTree:
+    def __init__(self, max_depth, min_samples):
+        self.max_depth = max_depth
+        self.min_samples = min_samples
+        self.root = None
         
-def most_frequent_value(y):
-    values, counts = np.unique(y, return_counts=True)
-    return values[np.argmax(counts)].astype(bool)
+    def binary_entropy(self, p):
+        if p == 0 or p == 1:
+            return 0
+        return -(p * np.log2(p) + (1 - p) * np.log2(1 - p))
     
-def build_decision_tree(X, y, depth=0, max_depth=5, min_samples=10):
-    if (max_depth and depth >= max_depth) or len(y) < min_samples or len(np.unique(y)) == 1:
-        return Node(value=most_frequent_value(y))
+    def most_frequent_value(self, y):
+        values, counts = np.unique(y, return_counts=True)
+        return values[np.argmax(counts)].astype(bool)
     
-    feature, threshold = best_split(X, y)
+    def compute_score(self, x, y):
+        thresholds = np.unique(x)
+        n_thresholds = len(thresholds)
+        n_samples = len(y)
+        best_score, best_threshold = float("inf"), None
+            
+        for i in range(n_thresholds - 1):
+            threshold = (thresholds[i] + thresholds[i + 1]) / 2
+            left = y[x <= threshold]
+            right = y[x > threshold]
+            n_left = len(left)
+            n_right = len(right)
+            
+            if n_left == 0 or n_right == 0:
+                continue
+            
+            w_left = n_left / n_samples
+            w_right = n_right / n_samples
+            
+            # y values are 0 and 1
+            p_left = left.mean()
+            p_right = right.mean()
+            
+            score = w_left * self.binary_entropy(p_left) + w_right * self.binary_entropy(p_right)
+            if score < best_score:
+                best_score = score
+                best_threshold = threshold
+                
+        return best_score, best_threshold
     
-    # in case there's a bug in the code
-    if feature is None or threshold is None:
-        return Node(value=most_frequent_value(y))
+    def best_split(self, X, y):
+        best_feature, best_threshold = None, None
+        best_score = float("inf")
+        
+        n_features = X.shape[1]
+        
+        for feature in range(n_features):
+            score, threshold = self.compute_score(X[:, feature], y)
+            if score < best_score:
+                best_score = score
+                best_feature = feature
+                best_threshold = threshold
+        
+        return best_feature, best_threshold
     
-    left_mask = X[:, feature] <= threshold
-    right_mask = ~left_mask
+    def build_decision_tree(self, X, y, depth=0):
+        if (self.max_depth and depth >= self.max_depth) or len(y) < self.min_samples or len(np.unique(y)) == 1:
+            return Node(value=self.most_frequent_value(y))
+        
+        feature, threshold = self.best_split(X, y)
+        
+        # in case there's a bug in the code
+        if feature is None or threshold is None:
+            return Node(value=self.most_frequent_value(y))
+        
+        left_mask = X[:, feature] <= threshold
+        right_mask = ~left_mask
+        
+        left = self.build_decision_tree(X[left_mask], y[left_mask], depth + 1)
+        right = self.build_decision_tree(X[right_mask], y[right_mask], depth + 1)
+        
+        return Node(feature=feature, threshold=threshold, left=left, right=right)
     
-    left = build_decision_tree(X[left_mask], y[left_mask], depth + 1, max_depth, min_samples)
-    right = build_decision_tree(X[right_mask], y[right_mask], depth + 1, max_depth, min_samples)
-    
-    return Node(feature=feature, threshold=threshold, left=left, right=right)
+    def fit(self, X, y):
+        unique = np.unique(y)
+        if not set(unique).issubset({0,1}):
+            raise ValueError("Only binary labels {0,1} are supported.")
 
-def predict_tree(x, node):
-    if node.value is not None:
-        return node.value
-    if x[node.feature] <= node.threshold:
-        return predict_tree(x, node.left)
-    return predict_tree(x, node.right)
-
-def predict_tree_batch(X, root):
-    return np.array([predict_tree(x, root) for x in X])
-
-def get_validation(X_train, y_train, info):
-    max_depth = info["max_depth"]
-    min_samples = info["min_samples"]
-    X_tr, X_val, y_tr, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+        self.root = self.build_decision_tree(X, y)
+        
+    def predict(self, x, node):
+        if node.value is not None:
+            return node.value
+        if x[node.feature] <= node.threshold:
+            return self.predict(x, node.left)
+        return self.predict(x, node.right)
     
-    root = build_decision_tree(X_tr, y_tr, max_depth=max_depth, min_samples=min_samples)
-    y_train_predict = predict_tree_batch(X_tr, root)
-    y_val_predict = predict_tree_batch(X_val, root)
+    def predict_batch(self, X):
+        if self.root is None:
+            raise ValueError("The tree has not been trained yet.") 
+        return np.array([self.predict(x, self.root) for x in X])
     
-    print(f"accuracy | train:{np.mean(y_train_predict == y_tr)} | validation: {np.mean(y_val_predict == y_val)}")
+    def fit_with_validation(self, X, y, test_size=0.2, random_state=42):
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-def get_result(X_train, y_train, X_test, passenger_id, info):
-    max_depth = info["max_depth"]
-    min_samples = info["min_samples"]
-    root = build_decision_tree(X_train, y_train, max_depth=max_depth, min_samples=min_samples)
-    y_predict = predict_tree_batch(X_test, root)
+        self.fit(X_train, y_train)
+        
+        y_train_predict = self.predict_batch(X_train)
+        y_val_predict = self.predict_batch(X_val)
+        
+        print(f"accuracy | train:{np.mean(y_train_predict == y_train) * 100:.2f} | validation: {np.mean(y_val_predict == y_val) * 100:.2f}")
+
+def get_validation(X_train, y_train, model: DecisionTree):
+    
+    model.fit_with_validation(X_train, y_train)
+
+def get_result(X_train, y_train, X_test, passenger_id, model):
+    
+    model.fit(X_train, y_train)
+    y_predict = model.predict_batch(X_test)
     
     df_out = pd.DataFrame({
         "PassengerId": passenger_id,
@@ -169,13 +186,10 @@ def get_result(X_train, y_train, X_test, passenger_id, info):
 def main():
     X_train, y_train, X_test, passenger_id = read_data("data/train.csv", "data/test.csv")
     
-    info = {
-        "max_depth": 8,
-        "min_samples": 20,
-    }
+    model = DecisionTree(max_depth=8, min_samples=20)
 
-    get_result(X_train, y_train, X_test, passenger_id, info)
-    # get_validation(X_train, y_train, info)
+    get_result(X_train, y_train, X_test, passenger_id, model)
+    # get_validation(X_train, y_train, model)
     
 if __name__ == "__main__":
     main()
