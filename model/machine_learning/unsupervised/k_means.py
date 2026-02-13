f78c776ba1cbe0ec8d09934a6e2b6d1930d889e6
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-02-13 18:44:02 +0700

    1st optimization of the k-means using vectorization

--- model/machine_learning/unsupervised/k_means.py
+++ model/machine_learning/unsupervised/k_means.py
@@ -3,9 +3,6 @@ import numpy as np
 class KMeans:
     def __init__(self):
         self.clusters = None
-        
-    def distance(self, x1, x2):
-        return np.sum((x1 - x2)**2)
     
     def get_clusters(self):
         return self.clusters
@@ -21,37 +18,26 @@ class KMeans:
             if i == k - 1:
                 break
             
-            next_cluster = None
-            max_d = -1
-            for j in range(m):
-                dist = self.distance(cluster, X[j])
-                if D[j] > dist:
-                    D[j] = dist
-                if D[j] > max_d:
-                    max_d = D[j]
-                    next_cluster = X[j]
-                    
-            cluster = next_cluster
-            
+            dists = np.sum((X - cluster)**2, axis=1)
+            D = np.minimum(D, dists)
+            cluster = X[np.argmax(D)]
+                                
         return new_clusters
     
-    def get_index_clusters(self, X, clusters, k, m):
-        index = np.zeros(m, dtype=int)
+    def get_index_clusters(self, X, clusters):
         
-        for i in range(m):
-            min_dist = np.inf
-            index_cluster = -1
-            for j in range(k):
-                dist = self.distance(X[i], clusters[j])
-                if dist < min_dist:
-                    min_dist = dist
-                    index_cluster = j
-            index[i] = index_cluster
-            
+        # diff[i][j][k] = X[i][k] - clusters[j][k]
+        # (m, f) - (k, f)
+        # = (m, 1, f) - (1, k, f)
+        # = (m, k, f) == diff.shape
+        
+        diff = X[:, None, :] - clusters[None, :, :]
+        
+        distances = np.sum(diff**2, axis=2)  
+        
+        index = np.argmin(distances, axis=1)
+
         return index
-    
-    def compute_mean(self, X):
-        return np.mean(X, axis=0)
     
     def move_clusters(self, X, clusters, k, m, max_iters, in_place=True):
         if not in_place:
@@ -63,69 +49,68 @@ class KMeans:
             if not updated:
                 break
             updated = False
-            # improvement needed
-            index_clusters = self.get_index_clusters(X, clusters, k, m)
+            index_clusters = self.get_index_clusters(X, clusters)
+            
+            if np.array_equal(index_clusters, index):
+                break
+            
             index = index_clusters
             
             for j in range(k):
-                points = X[index_clusters == j]
-                # need improvement
-                if len(points) == 0:
+                mask = (index_clusters == j)
+                if not np.any(mask):
+                    clusters[j] = X[np.random.randint(0, m)]
+                    updated = True
                     continue
-                next_clusters = self.compute_mean(points)
+                next_clusters = X[mask].mean(axis=0)
                 if not updated and not np.allclose(clusters[j], next_clusters):
                     updated = True
                 clusters[j] = next_clusters
         
         return clusters, index
             
-    def compute_score(self, X, index, k, m):
+    def compute_score(self, dist_matrix, index, k, m):
         sum_s = 0
+        cluster_masks = [index == c for c in range(k)]
         
         for i in range(m):
             # compute a
-            cluster_size = 0
             current_index_cluster = index[i]
-            total_dist = 0
-            for j in range(m):
-                if i == j:
-                    continue
-                if index[j] == current_index_cluster:
-                    cluster_size += 1
-                    total_dist += self.distance(X[i], X[j])
-                    
-            a = total_dist / cluster_size if cluster_size > 0 else 0
+            same_mask = cluster_masks[current_index_cluster] & (np.arange(m) != i)
+            a = np.mean(dist_matrix[i, same_mask]) if np.any(same_mask) else 0
             
             # compute b
-            other_dists = np.zeros((k, 2))
-            other_dists[current_index_cluster] = (np.inf, 0)
-            for j in range(m):
-                other_index_cluster = index[j]
-                if other_index_cluster == current_index_cluster:
-                    continue
-                dist = self.distance(X[i], X[j])
-                other_dists[other_index_cluster] += (dist, 1)
-                
             b = np.inf
-            for other_dist in other_dists:
-                if other_dist[1] == 0:
+            for c in range(k):
+                if c == current_index_cluster:
                     continue
-                b = min(b, other_dist[0] / other_dist[1])
+                other_mask = cluster_masks[c]
+                b = min(b, np.mean(dist_matrix[i, other_mask]))
         
             den = max(b, a)
             if den > 0:
                 sum_s += (b - a) / den
     
         return sum_s / m
+    
+    def compute_dist_matrix(self, X):
+        # diff[i][j][k] = X[i][k] - X[j][k]
+        diff = X[:, None, :] - X[None, :, :]
+        
+        # dist_matrix[i][j] = self.distance(X[i], X[j])
+        dist_matrix = np.sum(diff**2, axis=2)
+        
+        return dist_matrix
         
     def fit(self, X, max_iters=100):
         m = X.shape[0]
         self.clusters = None
         best_score = -1
+        dist_matrix = self.compute_dist_matrix(X)
         for k in range(2, min(m, 10)):
             clusters = self.init_clusters(X, k, m)
             _, index = self.move_clusters(X, clusters, k, m, max_iters)
-            score = self.compute_score(X, index, k, m)
+            score = self.compute_score(dist_matrix, index, k, m)
             if score > best_score:
                 best_score = score
                 self.clusters = clusters.copy()
