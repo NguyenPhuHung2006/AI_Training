787129b75d4eac07c0cfcc0de9e1e624624884eb
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-02-20 23:02:40 +0700

    refactor the gradient boosting code

--- model/machine_learning/supervised/gradient_boosting.py
+++ model/machine_learning/supervised/gradient_boosting.py
@@ -2,91 +2,46 @@ import numpy as np
 from abc import ABC, abstractmethod
 from sklearn.model_selection import train_test_split
 
-class GB_BaseLoss(ABC):
+class GB_Base(ABC):
     
-    @abstractmethod
-    def init_prediction(self, y):
-        pass
+    class Node:
+        def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+            self.feature = feature
+            self.threshold = threshold
+            self.left = left
+            self.right = right
+            self.value = value
     
-    @abstractmethod
-    def gradient(self, y, y_pred):
-        pass
-
-    @abstractmethod
-    def hessian(self, y, y_pred):
-        pass
-    
-    @abstractmethod
-    def leaf_value(self, gradients, hessians, lambda_):
-        pass
-    
-    @abstractmethod
-    def evaluate(self, y, y_pred):
-        pass
-    
-class GB_MSE(GB_BaseLoss):
-    
-    def init_prediction(self, y):
-        return np.mean(y)
-    
-    def gradient(self, y, y_pred):
-        return y_pred - y   
-
-    def hessian(self, y, y_pred):
-        return np.ones_like(y)
-
-    def leaf_value(self, gradients, hessians, lambda_):
-        return - np.sum(gradients) / (np.sum(hessians) + lambda_ + 1e-15)
-    
-    def evaluate(self, y, y_pred):
-        return 0.5 * np.mean((y - y_pred)**2)
-    
-class GB_BCE(GB_BaseLoss):
-    
-    def sigmoid(self, x):
-        x = np.clip(x, -500, 500)
-        return 1 / (1 + np.exp(-x))
-
-    def init_prediction(self, y):
-        mean_y = np.mean(y)
-        eps = 1e-15
-        mean_y = np.clip(mean_y, eps, 1 - eps)
-        return np.log(mean_y / (1 - mean_y))
-
-    def gradient(self, y, y_pred):
-        p = self.sigmoid(y_pred)
-        return p - y
-
-    def hessian(self, y, y_pred):
-        p = self.sigmoid(y_pred)
-        return p * (1 - p)
-
-    def leaf_value(self, gradients, hessians, lambda_):
-        return - np.sum(gradients) / (np.sum(hessians) + lambda_ + 1e-15)
-    
-    def predict_label(self, y_pred):
-        proba = self.sigmoid(y_pred)
-        return (proba >= 0.5).astype(int)
-    
-    def evaluate(self, y, y_pred):
-        preds = self.predict_label(y_pred)
-        y = y.astype(int)
-        return np.mean(preds == y)
-        
-class GB_Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
-        self.feature = feature
-        self.threshold = threshold
-        self.left = left
-        self.right = right
-        self.value = value
-        
-class GB_DecisionTree:
-    def __init__(self, max_depth, min_samples):
+    def __init__(self, loss, learning_rate: float, max_depth, min_samples, lambda_=0, n_trees=100):
+        self.loss = loss
+        self.learning_rate = learning_rate
         self.max_depth = max_depth
         self.min_samples = min_samples
-        self.root = None
-        
+        self.lambda_ = lambda_
+        self.n_trees = n_trees
+        self.trees = []
+        self.init_pred = None
+    
+    @abstractmethod
+    def init_prediction(self, y):
+        pass
+    
+    @abstractmethod
+    def gradient(self, y, y_pred):
+        pass
+    
+    @abstractmethod
+    def hessian(self, y, y_pred):
+        pass
+    
+    @abstractmethod
+    def leaf_value(self, gradients, hessians, lambda_):
+        pass
+    
+    @abstractmethod
+    def evaluate(self, y, y_pred):
+        pass
+    
     def gain(self, g_left, h_left, g_right, h_right, g_total, h_total, lambda_):
         eps = 1e-15
 
@@ -94,7 +49,7 @@ class GB_DecisionTree:
         right_score = (g_right ** 2) / (h_right + lambda_ + eps)
         parent_score = (g_total ** 2) / (h_total + lambda_ + eps)
 
-        return left_score + right_score - parent_score
+        return 0.5 * (left_score + right_score - parent_score)
     
     def compute_score(self, x, gradients, hessians, lambda_):
         thresholds = np.unique(x)
@@ -151,17 +106,16 @@ class GB_DecisionTree:
                 best_threshold = threshold
 
         return best_feature, best_threshold, best_gain
-
     
-    def build_decision_tree(self, X, gradients, hessians, loss, lambda_, depth=0):
+    def build_decision_tree(self, X, gradients, hessians, lambda_, depth=0):
         if depth >= self.max_depth or X.shape[0] < self.min_samples:
-            return GB_Node(value=loss.leaf_value(gradients, hessians, lambda_))
+            return self.Node(value=self.leaf_value(gradients, hessians, lambda_))
         
         feature, threshold, gain = self.best_split(X, gradients, hessians, lambda_)
         
         # in case there's a bug in the code
         if feature is None or threshold is None or gain <= 0:
-            return GB_Node(value=loss.leaf_value(gradients, hessians, lambda_))
+            return self.Node(value=self.leaf_value(gradients, hessians, lambda_))
         
         left_mask = X[:, feature] <= threshold
         right_mask = ~left_mask
@@ -170,20 +124,17 @@ class GB_DecisionTree:
             X[left_mask], 
             gradients[left_mask], 
             hessians[left_mask], 
-            loss, lambda_, depth + 1)
+            lambda_, depth + 1)
         
         right = self.build_decision_tree(
             X[right_mask], 
             gradients[right_mask], 
             hessians[right_mask], 
-            loss, lambda_, depth + 1)
+            lambda_, depth + 1)
         
-        return GB_Node(feature=feature, threshold=threshold, left=left, right=right)
+        return self.Node(feature=feature, threshold=threshold, left=left, right=right)
     
-    def fit(self, X, gradients, hessians, loss, lambda_):
-        self.root = self.build_decision_tree(X, gradients, hessians, loss, lambda_)
-                
-    def _predict(self, x, node):
+    def _predict_tree(self, x, node: Node):
         while node.value is None:
             if x[node.feature] <= node.threshold:
                 node = node.left
@@ -191,46 +142,34 @@ class GB_DecisionTree:
                 node = node.right
         return node.value
     
+    def predict_tree(self, X, node: Node):
+        return np.array([self._predict_tree(x, node) for x in X])
+    
     def predict(self, X):
-        return np.array([self._predict(x, self.root) for x in X])
-
-class GradientBoosting:
-    def __init__(self, loss, learning_rate: float, max_depth, min_samples, lambda_=0, n_trees=100):
-        self.loss = loss
-        self.learning_rate = learning_rate
-        self.max_depth = max_depth
-        self.min_samples = min_samples
-        self.lambda_ = lambda_
-        self.n_trees = n_trees
-        self.trees = []
-        self.init_pred = None
+        y_pred = np.full(X.shape[0], self.init_pred, dtype=float)
         
+        for tree in self.trees:
+            y_pred += self.learning_rate * self.predict_tree(X, tree)
+            
+        return y_pred
+    
     def fit(self, X, y, check_every=None):
         self.trees = []
-        self.init_pred = self.loss.init_prediction(y)
+        self.init_pred = self.init_prediction(y)
         y_pred = np.full_like(y, self.init_pred, dtype=float)
         
         for i in range(self.n_trees):
-            gradients = self.loss.gradient(y, y_pred)
-            hessians = self.loss.hessian(y, y_pred)
+            gradients = self.gradient(y, y_pred)
+            hessians = self.hessian(y, y_pred)
 
-            tree = GB_DecisionTree(self.max_depth, self.min_samples)
-            tree.fit(X, gradients, hessians, self.loss, self.lambda_)
-            update = tree.predict(X)
+            tree = self.build_decision_tree(X, gradients, hessians, self.lambda_)
+            update = self.predict_tree(X, tree)
             y_pred += self.learning_rate * update
             self.trees.append(tree)
             
             if i > 0 and check_every is not None and i % max(1, check_every) == 0:
                 print(f"{i:03d} / {self.n_trees:04d}")
-            
-    def predict(self, X):
-        y_pred = np.full(X.shape[0], self.init_pred, dtype=float)
-        
-        for tree in self.trees:
-            y_pred += self.learning_rate * tree.predict(X)
-            
-        return y_pred
-                
+    
     def fit_with_validation(self, X, y, test_size=0.2, random_state=42, check_every=None):
         X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size, random_state=random_state)
         
@@ -240,6 +179,55 @@ class GradientBoosting:
         y_val_predict = self.predict(X_val)
         
         print(
-            f"train:{self.loss.evaluate(y_train, y_train_predict):.4f} | "
-            f"validation:{self.loss.evaluate(y_val, y_val_predict):.4f}"
+            f"train:{self.evaluate(y_train, y_train_predict):.4f} | "
+            f"validation:{self.evaluate(y_val, y_val_predict):.4f}"
         )   
+    
+class GB_Regression(GB_Base):
+    
+    def init_prediction(self, y):
+        return np.mean(y)
+    
+    def gradient(self, y, y_pred):
+        return y_pred - y   
+
+    def hessian(self, y, y_pred):
+        return np.ones_like(y)
+
+    def leaf_value(self, gradients, hessians, lambda_):
+        return - np.sum(gradients) / (np.sum(hessians) + lambda_ + 1e-15)
+    
+    def evaluate(self, y, y_pred):
+        return 0.5 * np.mean((y - y_pred)**2)
+    
+class GB_Classification(GB_Base):
+    
+    def sigmoid(self, x):
+        x = np.clip(x, -500, 500)
+        return 1 / (1 + np.exp(-x))
+
+    def init_prediction(self, y):
+        mean_y = np.mean(y)
+        eps = 1e-15
+        mean_y = np.clip(mean_y, eps, 1 - eps)
+        return np.log(mean_y / (1 - mean_y))
+
+    def gradient(self, y, y_pred):
+        p = self.sigmoid(y_pred)
+        return p - y
+
+    def hessian(self, y, y_pred):
+        p = self.sigmoid(y_pred)
+        return p * (1 - p)
+
+    def leaf_value(self, gradients, hessians, lambda_):
+        return - np.sum(gradients) / (np.sum(hessians) + lambda_ + 1e-15)
+    
+    def predict_label(self, y_pred):
+        proba = self.sigmoid(y_pred)
+        return (proba >= 0.5).astype(int)
+    
+    def evaluate(self, y, y_pred):
+        preds = self.predict_label(y_pred)
+        y = y.astype(int)
+        return np.mean(preds == y)
