972dcd0473a85d8578a30e08a9121d46d3d82f8d
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-03-20 18:33:49 +0700

    add callback

---
+++ ai_model/ai_model/deep_learning/nn_torch/callback/EarlyStopping.py
@@ -0,0 +1,25 @@
+import Callback
+
+class EarlyStopping(Callback):
+
+    def __init__(self, patience=5):
+        self.patience = patience
+        self.best_loss = float("inf")
+        self.counter = 0
+
+    def on_epoch_end(self, trainer, logs):
+
+        val_loss = logs.get("val_loss")
+
+        if val_loss is None:
+            return
+
+        if val_loss < self.best_loss:
+            self.best_loss = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+
+            if self.counter >= self.patience:
+                print("Early stopping triggered")
+                trainer.stop_training = True
---
+++ ai_model/ai_model/deep_learning/nn_torch/callback/ModelCheckpoint.py
@@ -0,0 +1,19 @@
+import Callback
+
+class ModelCheckpoint(Callback):
+
+    def __init__(self, path="best_model.pth"):
+        self.path = path
+        self.best_loss = float("inf")
+
+    def on_epoch_end(self, trainer, logs):
+
+        val_loss = logs.get("val_loss")
+
+        if val_loss is None:
+            return
+
+        if val_loss < self.best_loss:
+
+            self.best_loss = val_loss
+            trainer.save(self.path)
