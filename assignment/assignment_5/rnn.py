addfda01143dbe3315b7b0d122c0f8b8f44ae98a
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-03-28 10:30:34 +0700

    initialize the rnn file in assignment 5

---
+++ assignment/assignment_5/rnn.py
@@ -0,0 +1,13 @@
+from ai_model.deep_learning.nn_torch import RNN
+from ai_model.deep_learning.nn_torch import BaseModel
+from ai_model.deep_learning.nn_torch.callback import EarlyStopping, ModelCheckpoint
+import os
+import numpy as np
+import pandas as pd
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    print("completed")
