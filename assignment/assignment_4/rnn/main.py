257df92498f7416c7c6eccda47ad42ecf6eb7897
Nguyen Phu Hung <hung.phunguyen8888@gmail.com>
2026-04-03 10:49:12 +0700

    initialize the files for rnn folder in assignment 4

---
+++ assignment/assignment_4/rnn/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import torch
+import os
+from torch.nn.utils.rnn import pad_sequence
+from ai_model.deep_learning.nn_torch import TextCNN
+from ai_model.deep_learning.nn_torch.callback import EarlyStopping, ModelCheckpoint
+import numpy as np
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
+    print("completed")
